7841e1afe4d132466045502366dea753e384b569
Jasmine Gadie and Megan Lee <jasmine@>
2018-02-14 20:53:51 -0800

    Fixed subtract, divide and sort of multiply functions

--- js/scripts.js
+++ js/scripts.js
@@ -36,15 +36,25 @@ $(document).ready(function() {
   });
 });
 
-//$(document).ready(function() {
-//  $("form#divide").submit(function(event) {
-//    event.preventDefault();
-//    var number1 = parseInt($("#add1").val());
-//    var number2 = parseInt($("#add2").val());
-////    var result = divide(number1, number2);
-  //  $("#output").text(result);
-//  });
-//});
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+  $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#mulitply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+  $("#output").text(result);
+  });
+});
 // UI logic
 
 
