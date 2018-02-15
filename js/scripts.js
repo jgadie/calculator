397a03d814f04a44b5e353a4859399cd0cb74cd5
Jasmine Gadie and Megan Lee <megan@>
2018-02-14 20:33:13 -0800

    add, subtract, divide, multiply options

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+//biz logic
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+//$(document).ready(function() {
+//  $("form#divide").submit(function(event) {
+//    event.preventDefault();
+//    var number1 = parseInt($("#add1").val());
+//    var number2 = parseInt($("#add2").val());
+////    var result = divide(number1, number2);
+  //  $("#output").text(result);
+//  });
+//});
+// UI logic
+
+
+
+
+
+//$("button#subtract").click(function() {
+  //$("numbersubtract").append("result")
+  //});
+
+//$("button#multiply").click(function() {
+  //$("body").append("result")
+  //});
+
+//$("button#divide").click(function() {
+  //$("body").addClass("result")
+  //});
+//});
